8b147a8bbcb2c029dd98db5341fd3a2ad28796a0
Gaurav Bharti <dreamsparks@outlook.com>
2020-07-05 00:08:51 +0530

    added image support

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from "rollup-plugin-typescript2";
+import image from '@rollup/plugin-image';
 
 const packageJson = require('./package.json');
 
@@ -24,6 +25,7 @@ export default {
         peerDepsExternal(),
         resolve(),
         commonjs(),
+        image(),
         typescript({ useTsconfigDeclarationDir: true })
     ]
 };
